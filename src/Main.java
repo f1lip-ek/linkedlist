cc597384bb1d26befdd4b75d165f1c0397ee73d0
zitek2 <filipzitek@seznam.cz>
2025-11-28 10:53:13 +0100

    finally done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        MujSpojovySeznam seznam = new MujSpojovySeznam();
-        seznam.addFirst(3);
-        seznam.addFirst(4);
-        seznam.addFirst(1);
-        seznam.addFirst(2);
+        MujSpojovySeznam listik = new MujSpojovySeznam();
+        listik.addFirst(5);
+        listik.addFirst(6);
+        listik.addFirst(9);
+        listik.addFirst(1);
+        listik.addFirst(4);
+        listik.addFirst(20);
+        listik.addFirst(8);
+        listik.addFirst(3);
+        listik.addFirst(2);
 
-        System.out.println(seznam);
+        LinkedListIterator iterator = (LinkedListIterator) listik.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
 
 
 
